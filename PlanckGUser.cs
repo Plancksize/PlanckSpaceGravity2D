b940ea484f2fa8c7f7ae9c79b8c301f5cb741527
Plancksize <37113965+Plancksize@users.noreply.github.com>
2022-05-29 20:38:12 +0100

    Add files via upload

--- PlanckGUser.cs
+++ PlanckGUser.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
+using System;
+using System.Collections;
 
 namespace Plancksize
 {
     public class PlanckGUser : MonoBehaviour
     {
-        public bool gRotation = true;
+        public bool lookAt = true;
+        public float lookSmoothness;
+        public float rotationOffset;
         public float mass = 1;
 
         [Space(20)]
         [SerializeField]
         private PlanckGCaster gCaster;
 
+        [SerializeField]
         Transform bodyTransform;
         Collider2D bodyCollider;
         Rigidbody2D bodyRB;
@@ -26,16 +31,15 @@ namespace Plancksize
             bodyRB = GetComponent<Rigidbody2D>();
         }
 
+
+        private void Update()
+        {
+            
+        }
         private void FixedUpdate()
         {
-            if (gCaster != null)
-            {
-                if (!gCaster.gPool.Contains(bodyCollider))
-                    gCaster = null;
-
-                if (gRotation && gCaster != null)
-                    RotateWithGravity();
-            }
+            if (gCaster != null && !gCaster.gPool.Contains(bodyCollider))
+                gCaster = null;
         }
 
         //Calculate G-Force scale
@@ -58,22 +62,39 @@ namespace Plancksize
         public void GPull(PlanckGCaster caster, bool useMasses = false)
         {
             if (useMasses)
-            {
                 appliedForce = -(caster.mass * mass * -(caster.gravityConstant));
-            }
             else
                 appliedForce = caster.gravityConstant;
 
             Vector2 gForceDirection = caster.attractorTransform.position - bodyTransform.position;
             bodyRB.AddForce(gForceDirection.normalized * -appliedForce * 10 * gScale(caster) * Time.fixedDeltaTime);
+
+            if (lookAt)
+                StartCoroutine(SmoothLookAtGravity(caster, lookSmoothness));
         }
 
         //Apply rotation towards G-Force Center
-        private void RotateWithGravity()
+        /*
+        private void RotateWithGravity(PlanckGCaster caster)
         {
-            Vector2 distanceVector = gCaster.attractorTransform.position - bodyTransform.position;
+            Vector2 distanceVector = caster.attractorTransform.position - bodyTransform.position;
             float gAngle = Mathf.Atan2(distanceVector.y, distanceVector.x) * Mathf.Rad2Deg;
-            bodyTransform.rotation = Quaternion.AngleAxis(gAngle + 90, Vector3.forward);
+            bodyTransform.rotation = Quaternion.AngleAxis(gAngle + rotationOffset, Vector3.forward);
+        }
+        */
+
+        //Apply rotation towards G-Force Center with controlled smoothness
+        private IEnumerator SmoothLookAtGravity(PlanckGCaster caster, float smoothness = 60)
+        {
+            Vector2 distanceVector = caster.attractorTransform.position - bodyTransform.position;
+            float gAngle = Mathf.Atan2(distanceVector.y, distanceVector.x) * Mathf.Rad2Deg;
+            if(smoothness > 0)
+                bodyTransform.rotation = Quaternion.Lerp(bodyTransform.rotation, Quaternion.AngleAxis(gAngle + rotationOffset, Vector3.forward), smoothness * Time.fixedDeltaTime);
+            else
+                bodyTransform.rotation = Quaternion.AngleAxis(gAngle + rotationOffset, Vector3.forward);
+            Debug.Log("rotate - "+ bodyTransform.rotation);
+
+            yield return null;
         }
     }
 
